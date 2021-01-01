d2d782696c6faecf34e46a6b548e7d652c9f8645
farleybsd <farley.t.i@hotmail.com>
2021-01-01 12:43:54 -0300

    Adicionado Listas de modelagem

--- BaltaStore/BaltaStore.Domain/StoreContext/Entities/Customer.cs
+++ BaltaStore/BaltaStore.Domain/StoreContext/Entities/Customer.cs
@@ -1,10 +1,11 @@
 using BaltaStore.Domain.StoreContext.ValueObjects;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace BaltaStore.Domain.StoreContext.Entities
 {
     public class Customer
     {
+        private readonly IList<Address> _address;
         // SOLID
         public Customer
         (Name name,
@@ -16,14 +17,20 @@ namespace BaltaStore.Domain.StoreContext.Entities
             Document = document;
             Email = email;
             Phone = phone;
-            Address = new List<Address>();
+            _address = new List<Address>();
         }
         public Name Name { get; set; }
         public Document Document { get; private set; }
         public Email Email { get; set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Address { get; private set; }
+        public IReadOnlyCollection<Address> Address => _address.ToArray();
 
+        public void AddAddress(Address address)
+        {
+            //Valida Endereco
+            //Adc Endereco
+            _address.Add(address);
+        }
         public override string ToString()
         {
             return Name.ToString();
--- BaltaStore/BaltaStore.Domain/StoreContext/Entities/Order.cs
+++ BaltaStore/BaltaStore.Domain/StoreContext/Entities/Order.cs
@@ -1,33 +1,44 @@
 using BaltaStore.Domain.StoreContext.Enums;
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace BaltaStore.Domain.StoreContext.Entities
 {
     public class Order
     {
+        private readonly IList<OrderItem> _items;
+        private readonly IList<Delivery> _deliveries;
         public Order(Customer customer)
         {
             Customer = customer;
             Number = Guid.NewGuid().ToString().Replace("-","").Substring(0,8).ToUpper();
             CreateDate = DateTime.Now;
             Status = EOrderStatus.Created;
-            Items = new List<OrderItem>();
-            Deliveries = new List<Delivery>();
+            _items = new List<OrderItem>();
+            _deliveries = new List<Delivery>();
         }
 
         public Customer Customer { get; private set; } // 1 para 1
         public string Number { get; private set; }
         public DateTime CreateDate { get; private set; }
         public EOrderStatus Status { get; private set; }
-        public IReadOnlyCollection<OrderItem> Items { get; private set; } // 1 para n
-        public IReadOnlyCollection<Delivery> Deliveries { get; private set; } // Lista de leitura pode adc apenas usando o AddItem
+        public IReadOnlyCollection<OrderItem> Items => _items.ToArray();
+        public IReadOnlyCollection<Delivery> Deliveries => _deliveries.ToArray();
 
-        public void AddItem(OrderItem orderItem)
+        public void AddItem(OrderItem item)
         {
             // Valida Item
 
             //Adciona ao Pedido
+            _items.Add(item);
+        }
+
+        public void AddDelivery(Delivery delivery)
+        {
+            // Valida Item
+
+            //Adciona ao Pedido
+            _deliveries.Add(delivery);
         }
 
         // To Place An Order
