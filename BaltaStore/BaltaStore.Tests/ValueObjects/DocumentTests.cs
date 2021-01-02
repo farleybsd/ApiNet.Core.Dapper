14aacc869f9dd1dc5476ec680f4b3bd113df786b
farleybsd <farley.t.i@hotmail.com>
2021-01-02 17:02:14 -0300

    Testando VOs

---
+++ BaltaStore/BaltaStore.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,32 @@
+﻿using BaltaStore.Domain.StoreContext.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaltaStore.Tests.ValueObjects
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        private Document validDocument;
+        private Document inValidDocument;
+        public DocumentTests()
+        {
+            validDocument= new Document("11405263679");
+            inValidDocument = new Document("12345678910");
+        }
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentisNotValid()
+        {
+            
+            Assert.AreEqual(1, inValidDocument.Notifications.Count);
+            Assert.AreEqual(false, inValidDocument.IsValid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentisValid()
+        {
+            
+            Assert.AreEqual(0, validDocument.Notifications.Count);
+            Assert.AreEqual(true, validDocument.IsValid);
+        }
+    }
+}
---
+++ BaltaStore/BaltaStore.Tests/ValueObjects/NameTests.cs
@@ -0,0 +1,33 @@
+﻿using BaltaStore.Domain.StoreContext.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaltaStore.Tests.ValueObjects
+{
+    [TestClass]
+    public class NameTests
+    {
+        private Name invalidName;
+        private Name validaName;
+        public NameTests()
+        {
+            invalidName = new Name("", "Rufino");
+            validaName = new Name("Farley", "Rufino");
+        }
+        [TestMethod]
+        public void ShouldReturnNotificationWhenNameIsNotValid()
+        {
+            Assert.AreEqual(1, invalidName.Notifications.Count);
+            Assert.AreEqual(false, invalidName.IsValid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenNameIsValid()
+        {
+            Assert.AreEqual(0, validaName.Notifications.Count);
+            Assert.AreEqual(true, validaName.IsValid);
+        }
+    }
+}
