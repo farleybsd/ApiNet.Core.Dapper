1690a41a763ad0fd329f802dd81207da003db93b
farleybsd <farley.t.i@hotmail.com>
2021-01-09 18:11:40 -0300

    Repositórios

---
+++ BaltaStore/BaltaStore.Domain/StoreContext/Repositories/ICustomRepository.cs
@@ -0,0 +1,14 @@
+﻿using BaltaStore.Domain.StoreContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaltaStore.Domain.StoreContext.Repositories
+{
+   public interface ICustomRepository
+    {
+        bool checkDocument(string document);
+        bool checkEmail(string email);
+        void Save(Customer customer);
+    }
+}
