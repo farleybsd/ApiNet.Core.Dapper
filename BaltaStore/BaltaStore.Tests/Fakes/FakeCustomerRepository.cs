39d1ec73420357086e331bcb91566e360ab84d8d
farleybsd <farley.t.i@hotmail.com>
2021-01-15 20:53:03 -0300

    Mocks e Fakes

---
+++ BaltaStore/BaltaStore.Tests/Fakes/FakeCustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿using BaltaStore.Domain.StoreContext.Entities;
+using BaltaStore.Domain.StoreContext.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaltaStore.Tests.Fakes
+{
+   public class FakeCustomerRepository : ICustomRepository
+    {
+        public bool checkDocument(string document)
+        {
+            return false;
+        }
+
+        public bool checkEmail(string email)
+        {
+            return false;
+        }
+
+        public void Save(Customer customer)
+        {
+           
+        }
+    }
+}
---
+++ BaltaStore/BaltaStore.Tests/Handlers/CustomerHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Input;
+using BaltaStore.Domain.StoreContext.Handlers;
+using BaltaStore.Tests.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaltaStore.Tests.Handlers
+{
+    [TestClass]
+   public class CustomerHandlerTests
+    {
+        [TestMethod]
+        public void ShouldRegisterCustomerWhenIsValid()
+        {
+            var command = new CreateCustomerCommand();
+            command.FirtsName = "Farley";
+            command.LastName = "Rufino";
+            command.Document = "11405263679";
+            command.Email = "Farley.t.i@hotmail.com";
+            command.Phone = "32123626";
+
+            Assert.AreEqual(true, command.Valid());
+
+            var handler = new CustomerHandler(new FakeCustomerRepository(),new FakeEmailService());
+        }
+        
+    }
+}
