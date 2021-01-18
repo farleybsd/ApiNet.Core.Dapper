9a5720436aecc5b9f676567b2179ebf453f6d6ae
farleybsd <farley.t.i@hotmail.com>
2021-01-18 20:36:44 -0300

    Criando um controlador

---
+++ BaltaStore/BaltaStore.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaltaStore.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public object GetVersion()
+        {
+            return new {Version="Version 0.0.1" };
+        }
+    }
+}
