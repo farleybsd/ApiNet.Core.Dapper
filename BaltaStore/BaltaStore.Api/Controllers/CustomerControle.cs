3533f55e1cf58bdff73e870de37bae9a8f762e5e
farleybsd <farley.t.i@hotmail.com>
2021-01-23 17:19:51 -0300

    Testando os CRUD

--- BaltaStore/BaltaStore.Api/Controllers/CustomerControle.cs
+++ BaltaStore/BaltaStore.Api/Controllers/CustomerControle.cs
@@ -1,4 +1,6 @@
-﻿using BaltaStore.Domain.StoreContext.Entities;
+﻿using BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Input;
+using BaltaStore.Domain.StoreContext.Entities;
+using BaltaStore.Domain.StoreContext.ValueObjects;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,42 +15,78 @@ namespace BaltaStore.Api.Controllers
         [Route("customers")]
         public List<Customer> Get()
         {
-            return null;
+            var name = new Name("Farley","Rufino");
+            var document = new Document("11405263679");
+            var email = new Email("farlet.t.i@hotmail.com");
+            var customer = new Customer(name, document, email, "5533991057769");
+            var customers = new List<Customer>();
+            customers.Add(customer);
+
+            return customers;
         }
 
         [HttpGet]
         [Route("customers/{id}")]
         public Customer GetById(Guid id)
         {
-            return null;
+            var name = new Name("Farley", "Rufino");
+            var document = new Document("11405263679");
+            var email = new Email("farlet.t.i@hotmail.com");
+            var customer = new Customer(name, document, email, "5533991057769");
+            return customer;
         }
 
         [HttpGet]
         [Route("customers/{id}/orders")]
         public List<Order> GetOrders(Guid id)
         {
-            return null;
+            var name = new Name("Farley", "Rufino");
+            var document = new Document("11405263679");
+            var email = new Email("farlet.t.i@hotmail.com");
+            var customer = new Customer(name, document, email, "5533991057769");
+
+            var order = new Order(customer);
+
+            var mouse = new Product("Mouse Gamer", "Mouse Gamer", "mouse.jpg", 100M, 10);
+            var KeyBoard = new Product("Teclado Gamer", "Teclado Gamer", "Teclado.jpg", 100M, 10);
+            
+
+            order.AddItem(mouse, 5);
+            order.AddItem(KeyBoard, 5);
+
+            var orders = new List<Order>();
+            orders.Add(order);
+
+            return orders;
         }
 
         [HttpPost]
         [Route("customers")]
-        public Customer Post([FromBody]Customer customer)
+        public Customer Post([FromBody]CreateCustomerCommand command)
         {
-            return null;
+            var name = new Name(command.FirtsName, command.LastName);
+            var document = new Document(command.Document);
+            var email = new Email(command.Email);
+            var customer = new Customer(name, document, email, command.Phone);
+            return customer;
         }
 
         [HttpPut]
         [Route("customers/{id}")]
-        public Customer Put([FromBody] Customer customer)
+        public Customer Put([FromBody] CreateCustomerCommand command)
         {
-            return null;
+            var name = new Name(command.FirtsName, command.LastName);
+            var document = new Document(command.Document);
+            var email = new Email(command.Email);
+            var customer = new Customer(name, document, email, command.Phone);
+            return customer;
         }
 
         [HttpDelete]
         [Route("customers/{id}")]
-        public string Delete()
+        public object Delete()
         {
-            return null;
+            return new { message = "Cliente removido com sucesso!" };
         }
     }
 }
