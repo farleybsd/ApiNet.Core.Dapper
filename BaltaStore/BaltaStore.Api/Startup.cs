ecefb507cae27efe0f2bc59380294ef3646c2827
farleybsd <farley.t.i@hotmail.com>
2021-01-24 03:22:18 -0300

    Utilizando o AppSettings

--- BaltaStore/BaltaStore.Api/Startup.cs
+++ BaltaStore/BaltaStore.Api/Startup.cs
@@ -8,15 +8,26 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace BaltaStore.Api
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; set; }
+
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
             services.AddMvc();
 
             //services.AddResponseCompression(); // Adicionado Compressao nas respstas da Api
